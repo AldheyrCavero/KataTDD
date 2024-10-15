a363eba8b07477c7a2790d93b924a1d34b2d0ae4
Aldheyr Cavero <62060023@continental.edu.pe>
2024-10-15 16:30:10 -0500

    Caso 3: Calcular promedio del conjunto vacio (Verde)

--- src/logica/Conjunto.py
+++ src/logica/Conjunto.py
@@ -4,4 +4,8 @@ class Conjunto:
         self.__conjunto=conjunto
 
     def promedio(self):
+            if len(self.__conjunto)==1
+                return(sel.__Conjunto[0])
+            elif len(self.__conjunto)==2
+                return(self.__conjunto[0] + self.__conjunto[1])/2
             return None
--- src/tests/TestConjunto.py
+++ src/tests/TestConjunto.py
@@ -2,6 +2,17 @@ import unittest
 from.src.logica.Conjunto import Conjunto
 
 class TestConjunto(unittest.TestCase):
-    deff test_conjunto_vacio_retornaNone(self):
-        conjunto = Conjunto ([])
-        self.assertEqual(conjunto.promedio())
+    def test_conjunto_vacio_retornaNone(self):
+        conjunto = Conjunto([])
+
+    self.assertIsNone(conjunto.promedio())
+
+    def test_conjunto_unElemento_retornaValorUnicoElemento(self):
+        conjunto = Conjunto([5])
+
+    self.assertEqual(5, conjunto.promedio())
+
+    def test_conjunto_dosElementos_retornaPromedioElementos(self):
+        conjunto = Conjunto([5, 7])
+
+    self.assertEqual(6, conjunto.promedio())
